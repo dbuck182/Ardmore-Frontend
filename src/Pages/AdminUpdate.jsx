274782be70d5877048e95460f4d98decaaacb6cb
drewbuck <dbuck182@gmail.com>
2024-07-17 22:00:36 -0400

    Admin Page work

--- src/Pages/AdminUpdate.jsx
+++ src/Pages/AdminUpdate.jsx
@@ -36,8 +36,11 @@ const AdminUpdate = () => {
     return (
         <div className="relative">
             <NavBar/>
-            <div className="flex flex-col absolute items-center mt-20">
-                <form onSubmit={handleSubmit} className="space-y-5" id="updateForm">
+            <div className="flex flex-row justify-center align-center">
+
+            <div className="flex flex-col items-center mt-20">
+                <h2 className="text-2xl">Business Form</h2>
+                <form onSubmit={handleSubmit} className="flex flex-col space-y-5 border p-5" id="updateForm">
                     <label id="Bus-Name">Business Name</label>
                         <input className="bg-white border" name="name" type="text" value={formData.BusName} onChange={handleChange}></input>
                     <label id="address">Address</label>
@@ -46,14 +49,52 @@ const AdminUpdate = () => {
                         <input className="bg-white border" type="text" name="website" value={formData.website} onChange={handleChange}></input>
                     <select className="bg-white border" value={formData.type} name="type" onChange={handleChange}>
                         <option>Select an Option</option>
-                        <option value="RESTAURANT">Restaurant</option>
-                        <option value="BAR">Bar</option>
-                        <option value="DESSERT">Dessert</option>
+                        <option value="FOOD">Food</option>
+                        <option value="RETAIL">Retail</option>
+                        <option value="HEALTHBEAUTY">Health & Beauty</option>
                     </select>
                     <button type="submit">Submit</button>
                 </form>
             </div>
-            
+            <div className="flex flex-col items-center mt-20">
+                <h2 className="text-2xl">Event Form</h2>
+                <form onSubmit={handleSubmit} className="flex flex-col space-y-5 border p-5" id="EventForm">
+                    <label id="Event-Name">Event Name</label>
+                        <input className="bg-white border" name="name" type="text" value={formData.BusName} onChange={handleChange}></input>
+                    <label id="address">Address</label>
+                        <input className="bg-white border" name="address"type="text" value={formData.address} onChange={handleChange}></input>
+                    <label id="website">Website</label>
+                        <input className="bg-white border" type="text" name="website" value={formData.website} onChange={handleChange}></input>
+                    <select className="bg-white border" value={formData.type} name="type" onChange={handleChange}>
+                        <option>Select an Option</option>
+                        <option value="FOOD">Food</option>
+                        <option value="RETAIL">Retail</option>
+                        <option value="HEALTHBEAUTY">Health & Beauty</option>
+                    </select>
+                    <label for="event-time">Date/Time</label>
+                    <input id="event-time" type="datetime-local"></input>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <div className="flex flex-col items-center mt-20">
+                <h2 className="text-2xl">Discount Form</h2>
+                <form onSubmit={handleSubmit} className="flex flex-col space-y-5 border p-5" id="EventForm">
+                    <label id="Event-Name">Event Name</label>
+                        <input className="bg-white border" name="name" type="text" value={formData.BusName} onChange={handleChange}></input>
+                    <label id="address">Address</label>
+                        <input className="bg-white border" name="address"type="text" value={formData.address} onChange={handleChange}></input>
+                    <label id="website">Website</label>
+                        <input className="bg-white border" type="text" name="website" value={formData.website} onChange={handleChange}></input>
+                    <select className="bg-white border" value={formData.type} name="type" onChange={handleChange}>
+                        <option>Select an Option</option>
+                        <option value="FOOD">Food</option>
+                        <option value="RETAIL">Retail</option>
+                        <option value="HEALTHBEAUTY">Health & Beauty</option>
+                    </select>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            </div>
         </div>
         
     )
---
+++ src/Pages/SignUpPage.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react"
+
+const SignUpPage = () => {
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("")
+    
+    
+
+    return (
+        <h1>Sign Up</h1>
+    )
+}
+
+export default SignUpPage

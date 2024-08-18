4948858004a246b3b03d924054691359cb41f806
Drew Buck <dbuck182@gmail.com>
2024-08-18 17:15:28 -0400

    front end fixes

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -14,7 +14,7 @@ const Homepage = () => {
             <div className='flex flex-row items-center justify-items-center'>
                 <img className='z-0 blur-sm w-screen'src={Ardmore}></img>
                 <div className='z-10 absolute flex flex-row justify-center content-center justify-items-center items-center'>
-                    <h2 className="text-3xl font-bold w-1/2 text-white bg-Haverford-red">
+                    <h2 className="lg:text-3xl sm:text-2xl font-bold w-1/2 text-white bg-Haverford-red px-5">
                         Ardmore is booming with <br />new restaurants and shops <br/>
                     </h2>
                     <img className='w-1/2' src={BlackSquirrelDiscount} alt="Black Squirrel Discount Logo"></img>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,15 @@ const NavBar = () => {
     return (
         <nav className="bg-white shadow-lg top-0 left-0 right-0 z-20 py-4 min-w-full px-2">
             <div className="flex flex-row items-center justify-around">
-                <h1 className="text-3xl">
+                <h2 className="sm:text-3xl">
                     Black Squirrel Discount
-                </h1>
+                </h2>
                 <ul className="flex flex-row">
                     <li className={classes.listclass}>
-                        <Link to={'/'}>Home</Link>
+                        <Link to={'/'} className="text-Haverford-red sm:text-2xl">Home</Link>
                     </li>
                     <li className={classes.listclass}>
-                        <Link to={'/login'}>Admin Login</Link>
+                        <Link to={'/login'} className="text-Haverford-red sm:text-2xl">Admin Login</Link>
                     </li>
                 </ul>
             </div>
@@ -24,7 +24,7 @@ const NavBar = () => {
 }
 
 const classes = {
-    listclass: "border border-slate-300 hover:border-slate-400 p-2"
+    listclass: "border border-slate-300 hover:border-slate-400 p-1 sm:p-2"
 }
 
 export default NavBar;
